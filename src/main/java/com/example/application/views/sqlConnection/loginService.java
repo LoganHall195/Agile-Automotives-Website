d75e22ad038e499b9ff6adce9ff08189720c95a6
acarranza9 <torresadrian059@gmail.com>
2022-11-03 18:50:58 -0500

    init login
    init login

---
+++ src/main/java/com/example/application/views/sqlConnection/loginService.java
@@ -0,0 +1,11 @@
+/*package com.example.application.views.sqlConnection;
+import com.example.application.views.sqlConnection.sqlConnection;
+public class loginService 
+{
+    public void loginTable(){
+        String string = "SELECT email, password FROM users";
+    sqlConnection sq = new sqlConnection(); 
+    }
+}*/
+
+
