69c952dd285201e99e63cb58b1599c4f949e1ef7
acarranza9 <torresadrian059@gmail.com>
2022-11-06 18:03:43 -0600

    Update cars.js

--- js/cars.js
+++ js/cars.js
@@ -9,6 +9,16 @@ const auth = getAuth(app);
 const carName1 = document.getElementById("Car1name");
 const database = ref(getDatabase());
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAKGYcsh-b050TlfBEZSoPdAhCD_P5zxC8",
+    authDomain: "softwaree-group7.firebaseapp.com",
+    projectId: "softwaree-group7",
+    storageBucket: "softwaree-group7.appspot.com",
+    messagingSenderId: "983507508483",
+    appId: "1:983507508483:web:d9ca8d329ac6142443dd36",
+    measurementId: "G-SQ2B4TL68L",
+    databaseURL: "https://softwaree-group7-default-rtdb.firebaseio.com",
+  };
 
 get(child(database, `/Cars/Car1/carName`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -22,7 +32,7 @@ get(child(database, `/Cars/Car1/carName`)).then((snapshot) => {
 
 
   $(document).ready(function() {
-    html = "Hello There!";
+    //html = "Hello There!";
     document.getElementById("Car1name").outerHTML = html
 });
   
